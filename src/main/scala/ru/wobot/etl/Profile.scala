8486ee6d6370bfa612c7cb58ae801465c24bb141
Kviz <kviz@outlook.com>
2016-05-25 18:36:12 +0300

    Read nutch segment

---
+++ src/main/scala/ru/wobot/etl/Profile.scala
@@ -0,0 +1,8 @@
+package ru.wobot.etl
+
+/**
+  * Created by kviz on 5/25/2016.
+  */
+class Profile {
+
+}
--- src/main/scala/ru/wobot/etl/SegmentExporter.scala
+++ src/main/scala/ru/wobot/etl/SegmentExporter.scala
@@ -1,5 +1,24 @@
 package ru.wobot.etl
 
+import java.lang.Iterable
+
+import org.apache.flink.api.common.functions.{FlatMapFunction, GroupReduceFunction}
+import org.apache.flink.api.java.ExecutionEnvironment
+import org.apache.flink.api.java.functions.KeySelector
+import org.apache.flink.api.scala.hadoop.mapreduce.HadoopInputFormat
+import org.apache.flink.util.Collector
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.io.{Text, Writable}
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat
+import org.apache.nutch.crawl.{CrawlDatum, Inlinks, NutchWritable}
+import org.apache.nutch.metadata.{Metadata, Nutch}
+import org.apache.nutch.parse.{ParseData, ParseText}
+import org.apache.nutch.protocol.Content
+import org.apache.nutch.segment.SegmentChecker
+import org.apache.nutch.util.HadoopFSUtil
+
+import scala.collection.JavaConverters._
+
 /**
   * Licensed to the Apache Software Foundation (ASF) under one
   * or more contributor license agreements.  See the NOTICE file
@@ -18,11 +37,6 @@ package ru.wobot.etl
   * limitations under the License.
   */
 
-import org.apache.flink.api.scala._
-import org.apache.flink.api.scala.hadoop.mapreduce.HadoopInputFormat
-import org.apache.hadoop.io.{Text, Writable}
-import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat
-
 /**
   * Implements the "WordCount" program that computes a simple word occurrence histogram
   * over some sample data
@@ -35,13 +49,75 @@ import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat
   */
 object SegmentExporter {
   def main(args: Array[String]) {
-
-    // set up the execution environment
     val env = ExecutionEnvironment.getExecutionEnvironment
     val job = org.apache.hadoop.mapreduce.Job.getInstance()
     val format = new HadoopInputFormat[Text, Writable](new SequenceFileInputFormat[Text, Writable], classOf[Text], classOf[Writable], job)
     val input = env.createInput(format)
-    //val count = groupBy.count();
 
+    val dirPath = "C:\\crawl\\segments"
+    val dir: Path = new Path(dirPath)
+    val fs = dir.getFileSystem(job.getConfiguration);
+    val fstats = fs.listStatus(dir, HadoopFSUtil.getPassDirectoriesFilter(fs));
+    val files = HadoopFSUtil.getPaths(fstats);
+    for (p <- files)
+      if (SegmentChecker.isIndexable(p, fs)) {
+        println("Add " + p)
+        org.apache.hadoop.mapreduce.lib.input.FileInputFormat.addInputPath(job, new Path(p, CrawlDatum.FETCH_DIR_NAME))
+        org.apache.hadoop.mapreduce.lib.input.FileInputFormat.addInputPath(job, new Path(p, CrawlDatum.PARSE_DIR_NAME))
+        org.apache.hadoop.mapreduce.lib.input.FileInputFormat.addInputPath(job, new Path(p, ParseData.DIR_NAME))
+        org.apache.hadoop.mapreduce.lib.input.FileInputFormat.addInputPath(job, new Path(p, ParseText.DIR_NAME))
+      }
+
+    val map = input.flatMap(new FlatMapFunction[(Text, Writable), (Text, NutchWritable)] {
+      override def flatMap(value: (Text, Writable), out: Collector[(Text, NutchWritable)]): Unit =
+        value._2 match {
+          case datum: CrawlDatum =>
+            if (!(datum.getStatus() == CrawlDatum.STATUS_LINKED || datum.getStatus() == CrawlDatum.STATUS_SIGNATURE || datum.getStatus() == CrawlDatum.STATUS_PARSE_META))
+              out.collect(new Tuple2[Text, NutchWritable](value._1, new NutchWritable(value._2)))
+          case c: Content => ()
+          case i: Inlinks => ()
+          case _ => out.collect(new Tuple2[Text, NutchWritable](value._1, new NutchWritable(value._2)))
+        }
+    })
+
+    val groupBy = map.groupBy(new KeySelector[(Text, NutchWritable), Text] {
+      override def getKey(value: (Text, NutchWritable)): Text = value._1
+    })
+
+    val result = groupBy.reduceGroup(new GroupReduceFunction[(Text, NutchWritable), (String, String, Post, Profile)] {
+      override def reduce(values: Iterable[(Text, NutchWritable)], out: Collector[(String, String, Post, Profile)]): Unit = {
+        var fetchDatum: CrawlDatum = null
+        var parseText: ParseText = null
+        var parseData: ParseData = null
+
+        for (elem <- values.asScala) {
+          val v = elem._2.get()
+          v match {
+            case c: CrawlDatum => fetchDatum = c
+            case p: ParseData => parseData = p
+            case p: ParseText => parseText = p
+            case _ => ()
+          }
+        }
+
+        if (parseData == null) return
+        if (!parseData.getStatus.isSuccess || fetchDatum.getStatus != CrawlDatum.STATUS_FETCH_SUCCESS) return
+        val contentMeta = parseData.getContentMeta;
+        val skipFromElastic: String = contentMeta.get("nutch.content.index.elastic.skip")
+        if (skipFromElastic != null && skipFromElastic.equals("1")) return
+        val crawlDate: String = contentMeta.get("nutch.content.fetch.time")
+        val segment = contentMeta.get(Nutch.SEGMENT_NAME_KEY);
+        val isSingleDoc: Boolean = !"true".equals(contentMeta.get("nutch.parse.result.is_multiple"))
+        if (isSingleDoc) {
+          val parseMeta: Metadata = parseData.getParseMeta
+
+        }
+
+        println("segment=" + segment)
+      }
+    })
+
+    //val count = groupBy.count();
+    println("Total=" + result.count())
   }
 }
