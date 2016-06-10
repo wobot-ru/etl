2458216e90d7741549d35e5bb41f4aae6412cd53
Kviz <kviz@outlook.com>
2016-06-10 15:17:05 +0300

    Remove main class inheritance

--- src/main/scala/ru/wobot/etl/flink/HBase.scala
+++ src/main/scala/ru/wobot/etl/flink/HBase.scala
@@ -10,95 +10,97 @@ import ru.wobot.etl.flink.HBaseConstants.Tables
 import ru.wobot.etl.flink.OutputFormat.HBaseOutputFormat
 import ru.wobot.etl.{DetailedOrWithoutAuthorPost, Post, Profile}
 
-object HBase extends App {
-
-  val env = ExecutionEnvironment.getExecutionEnvironment
-  val profilesToProcess = env.createInput(InputFormat.profileToProcess)
-  val postsToProcess = env.createInput(InputFormat.postToProcess)
-  val profiles = env.createInput(InputFormat.profilesStore)
-
-
-  val admin = new HBaseAdmin(HBaseConfiguration.create)
-
-  def disableTable(name: TableName) = {
-    if (admin.isTableEnabled(name))
-      admin.disableTable(name)
-  }
-
-  def truncateTable(name: TableName): Unit = {
-    disableTable(name)
-    admin.truncateTable(name, true)
-  }
-
-  updateProfileView(env, profilesToProcess, profiles, OutputFormat.profilesStore)
-  updatePostView(env, postsToProcess, profiles)
+object HBase {
+  def main(args: Array[String]): Unit = {
+    val env = ExecutionEnvironment.getExecutionEnvironment
+    val profilesToProcess = env.createInput(InputFormat.profileToProcess)
+    val postsToProcess = env.createInput(InputFormat.postToProcess)
+    val profiles = env.createInput(InputFormat.profilesStore)
 
 
-  def updateProfileView(env: ExecutionEnvironment, processing: DataSet[Profile], saved: DataSet[Profile], profilesStore: HBaseOutputFormat[Profile]) = {
-    val latest = processing.groupBy(x => x.url).sortGroup(x => x.crawlDate, Order.DESCENDING).first(1)
-    val toUpdate = latest.leftOuterJoin(saved).where(0).equalTo(0).apply((l: Profile, r: Profile, collector: Collector[Profile]) => {
-      if (r == null || r.crawlDate < l.crawlDate)
-        collector.collect(l)
-    })
+    val admin = new HBaseAdmin(HBaseConfiguration.create)
 
-    toUpdate.output(OutputFormat profilesStore)
-    env.execute("Update profiles")
+    def disableTable(name: TableName) = {
+      if (admin.isTableEnabled(name))
+        admin.disableTable(name)
+    }
 
-    truncateTable(HBaseConstants.Tables.PROFILE_TO_PROCESS)
-  }
+    def truncateTable(name: TableName): Unit = {
+      disableTable(name)
+      admin.truncateTable(name, true)
+    }
 
-  def updatePostView(env: ExecutionEnvironment, processing: DataSet[Post], profiles: DataSet[Profile]) = {
-    val latest = processing.groupBy(x => x.url).sortGroup(x => x.crawlDate, Order.DESCENDING).first(1)
-    val joined = latest
-      .leftOuterJoin(profiles)
-      .where(x => x.post.profileId)
-      .equalTo(x => x.url)
-      .apply((l: Post, r: Profile, collector: Collector[DetailedOrWithoutAuthorPost]) => {
-        if (r == null) {
-          collector.collect(DetailedOrWithoutAuthorPost(None, Some(l)))
-        }
-        else {
-          val p = l.post
-          val pr = r.profile
-          val post = new DetailedPostDto(id = p.id,
-            segment = p.segment,
-            crawlDate = p.crawlDate,
-            href = p.href,
-            source = p.href,
-            profileId = p.profileId,
-            smPostId = p.smPostId,
-            parentPostId = p.parentPostId,
-            body = p.body,
-            date = p.date,
-            engagement = p.engagement,
-            isComment = p.isComment,
-            profileCity = pr.city,
-            profileGender = pr.gender,
-            profileHref = pr.href,
-            profileName = pr.name,
-            reach = pr.reach,
-            smProfileId = pr.smProfileId)
-          collector.collect(DetailedOrWithoutAuthorPost(Some(post), None))
-        }
+    updateProfileView(env, profilesToProcess, profiles, OutputFormat.profilesStore)
+    updatePostView(env, postsToProcess, profiles)
+
+
+    def updateProfileView(env: ExecutionEnvironment, processing: DataSet[Profile], saved: DataSet[Profile], profilesStore: HBaseOutputFormat[Profile]) = {
+      val latest = processing.groupBy(x => x.url).sortGroup(x => x.crawlDate, Order.DESCENDING).first(1)
+      val toUpdate = latest.leftOuterJoin(saved).where(0).equalTo(0).apply((l: Profile, r: Profile, collector: Collector[Profile]) => {
+        if (r == null || r.crawlDate < l.crawlDate)
+          collector.collect(l)
       })
-    val unAuthorized = joined
-      .filter(p => p.withoutAuthor.isDefined)
-      .map(x => x.withoutAuthor.get)
 
-    val autorized = joined
-      .filter(p => p.withoutAuthor.isEmpty)
-      .map(x => x.detailed.get)
+      toUpdate.output(OutputFormat profilesStore)
+      env.execute("Update profiles")
 
-    autorized.output(OutputFormat.postsToES)
-    unAuthorized.output(OutputFormat.postsWithoutProfile)
-    env.execute("Update posts")
+      truncateTable(HBaseConstants.Tables.PROFILE_TO_PROCESS)
+    }
 
-    truncateTable(Tables.POST_TO_PROCESS)
-    env.createInput(InputFormat.postWithoutProfile)
-      .output(OutputFormat.postsToProces())
+    def updatePostView(env: ExecutionEnvironment, processing: DataSet[Post], profiles: DataSet[Profile]) = {
+      val latest = processing.groupBy(x => x.url).sortGroup(x => x.crawlDate, Order.DESCENDING).first(1)
+      val joined = latest
+        .leftOuterJoin(profiles)
+        .where(x => x.post.profileId)
+        .equalTo(x => x.url)
+        .apply((l: Post, r: Profile, collector: Collector[DetailedOrWithoutAuthorPost]) => {
+          if (r == null) {
+            collector.collect(DetailedOrWithoutAuthorPost(None, Some(l)))
+          }
+          else {
+            val p = l.post
+            val pr = r.profile
+            val post = new DetailedPostDto(id = p.id,
+              segment = p.segment,
+              crawlDate = p.crawlDate,
+              href = p.href,
+              source = p.href,
+              profileId = p.profileId,
+              smPostId = p.smPostId,
+              parentPostId = p.parentPostId,
+              body = p.body,
+              date = p.date,
+              engagement = p.engagement,
+              isComment = p.isComment,
+              profileCity = pr.city,
+              profileGender = pr.gender,
+              profileHref = pr.href,
+              profileName = pr.name,
+              reach = pr.reach,
+              smProfileId = pr.smProfileId)
+            collector.collect(DetailedOrWithoutAuthorPost(Some(post), None))
+          }
+        })
+      val unAuthorized = joined
+        .filter(p => p.withoutAuthor.isDefined)
+        .map(x => x.withoutAuthor.get)
 
-    env.execute("Restore un-joined posts")
+      val autorized = joined
+        .filter(p => p.withoutAuthor.isEmpty)
+        .map(x => x.detailed.get)
 
-    truncateTable(Tables.POST_WITHOUT_PROFILE)
+      autorized.output(OutputFormat.postsToES)
+      unAuthorized.output(OutputFormat.postsWithoutProfile)
+      env.execute("Update posts")
+
+      truncateTable(Tables.POST_TO_PROCESS)
+      env.createInput(InputFormat.postWithoutProfile)
+        .output(OutputFormat.postsToProces())
+
+      env.execute("Restore un-joined posts")
+
+      truncateTable(Tables.POST_WITHOUT_PROFILE)
+    }
   }
+
 }
--- src/main/scala/ru/wobot/etl/flink/Runner.scala
+++ src/main/scala/ru/wobot/etl/flink/Runner.scala
@@ -3,21 +3,24 @@ package ru.wobot.etl.flink
 import org.apache.flink.api.java.utils.ParameterTool
 import ru.wobot.etl.flink.nutch.Nutch
 
-object Runner extends App {
-  val params = ParameterTool.fromArgs(args)
+object Runner {
+  def main(args: Array[String]): Unit = {
+    val params = ParameterTool.fromArgs(args)
 
-  if (params.has("smokes"))
-    Smokes.main(args)
+    if (params.has("smokes"))
+      Smokes.main(args)
 
-  if (params.has("kafka")) {
-    println("Run kafka")
-    Kafka.main(args)
+    if (params.has("kafka")) {
+      println("Run kafka")
+      Kafka.main(args)
+    }
+    else {
+      if (params.has("nutch"))
+        Nutch.main(args)
+
+      if (params.has("hbase"))
+        HBase.main(args)
+    }
   }
-  else {
-    if (params.has("nutch"))
-      Nutch.main(args)
 
-    if (params.has("hbase"))
-      HBase.main(args)
-  }
 }
