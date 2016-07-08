fce98e756c848068f34b6391c53abf439c54f8a6
Kviz <kviz@outlook.com>
2016-07-08 23:15:11 +0300

    Fix bug. Not all the fields are store in "post-to-es0 table".

---
+++ src/main/scala/ru/wobot/etl/dto/DetailedPostDto.scala
@@ -0,0 +1,28 @@
+package ru.wobot.etl.dto
+
+import ru.wobot.etl.JsonUtil
+
+class DetailedPostDto(id: String,
+                      segment: String,
+                      crawlDate: String,
+                      href: String,
+                      source: String,
+                      profileId: String,
+                      smPostId: String,
+                      parentPostId: String,
+                      body: String,
+                      date: String,
+                      engagement: String,
+                      isComment: Boolean,
+                      val profileCity: String,
+                      val profileGender: String,
+                      val profileHref: String,
+                      val profileName: String,
+                      val reach: String,
+                      val smProfileId: String)
+  extends PostDto(id, segment, crawlDate, href, source, profileId, smPostId, parentPostId, body, date, engagement, isComment) {
+
+  override def toString: String = toJson()
+
+  override def toJson(): String = JsonUtil.toJson(this)
+}
--- src/main/scala/ru/wobot/etl/dto/PostDto.scala
+++ src/main/scala/ru/wobot/etl/dto/PostDto.scala
@@ -20,28 +20,3 @@ class PostDto(id: String,
 
   def toJson(): String = JsonUtil.toJson(this)
 }
-
-class DetailedPostDto(id: String,
-                      segment: String,
-                      crawlDate: String,
-                      href: String,
-                      source: String,
-                      profileId: String,
-                      smPostId: String,
-                      parentPostId: String,
-                      body: String,
-                      date: String,
-                      engagement: String,
-                      isComment: Boolean,
-                      profileCity: String,
-                      profileGender: String,
-                      profileHref: String,
-                      profileName: String,
-                      reach: String,
-                      smProfileId: String)
-  extends PostDto(id, segment, crawlDate, href, source, profileId, smPostId, parentPostId, body, date, engagement, isComment) {
-
-  override def toString: String = toJson()
-
-  override def toJson(): String = JsonUtil.toJson(this)
-}
