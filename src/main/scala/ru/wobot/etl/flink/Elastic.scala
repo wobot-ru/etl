eee51a4a14466959a312222ef27ea7dedafd217a
Kviz <kviz@outlook.com>
2016-07-09 01:14:08 +0300

    Implement the HBase exports to FS

--- src/main/scala/ru/wobot/etl/flink/Elastic.scala
+++ src/main/scala/ru/wobot/etl/flink/Elastic.scala
@@ -1,24 +1,50 @@
 package ru.wobot.etl.flink
 
 import org.apache.flink.api.common.restartstrategy.RestartStrategies
+import org.apache.flink.api.java.io.{TypeSerializerInputFormat, TypeSerializerOutputFormat}
 import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.api.scala.{ExecutionEnvironment, _}
+import org.apache.flink.core.fs.FileSystem.WriteMode
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.hadoop.mapred.JobConf
 import org.slf4j.{Logger, LoggerFactory}
 import ru.wobot.etl.DetailedPost
+import ru.wobot.etl._
+import ru.wobot.etl.flink.Params._
 
 object Elastic {
   private val LOGGER: Logger = LoggerFactory.getLogger(HBase.getClass.getName)
 
   def main(args: Array[String]): Unit = {
     LOGGER.info("Run hbase")
+
     val params = ParameterTool.fromArgs(args)
-    val env = ExecutionEnvironment.getExecutionEnvironment
-    env.getConfig.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, 120000))
-    env.getConfig.enableForceKryo()
+    val outDir = params.getRequired(HBASE_OUT_DIR)
+    if (!params.has(HBASE_EXPORT) && (!params.has(UPLOAD_TO_ES)))
+      throw new IllegalArgumentException("Missed parameter: \"--hbase-export\" or \"--upload-to-es\"")
 
-    val input = env.createInput(InputFormat.postToEs())
-    val take: Seq[DetailedPost] = input.collect().take(10)
-    println(take)
+    if (params.has(HBASE_EXPORT)) {
+      val env = ExecutionEnvironment.getExecutionEnvironment
+      env.getConfig.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, 120000))
+      env.getConfig.enableForceKryo()
 
+      val input: DataSet[DetailedPost] = env.createInput(InputFormat.postToEs()).rebalance().name("post-to-es")
+      input.write(new TypeSerializerOutputFormat[DetailedPost], outDir, WriteMode.OVERWRITE).name(s"export-to: $outDir")
+      env.execute("export-from-hbase-to-filesystem")
+    }
+    if (params.has(UPLOAD_TO_ES)) {
+      val fs = FileSystem.get(new JobConf())
+      if (!fs.exists(new Path(outDir))) throw new RuntimeException(s"Path not exist: $outDir")
+
+      val stream = StreamExecutionEnvironment.getExecutionEnvironment
+      stream.getConfig.enableForceKryo()
+      val posts: DataStream[DetailedPost] = stream
+        .readFile(new TypeSerializerInputFormat[DetailedPost](detailedPostTI), outDir)
+      posts.print()
+
+      stream.execute("upload-post-to-es")
+    }
   }
+
 }
--- src/main/scala/ru/wobot/etl/flink/Params.scala
+++ src/main/scala/ru/wobot/etl/flink/Params.scala
@@ -3,4 +3,11 @@ package ru.wobot.etl.flink
 object Params {
   val TOPIC_POST = "topic-post"
   val TOPIC_PROFILE = "topic-profile"
+
+  val DIR_UPLOAD_POST = "dir-upload-post"
+  val DETAILED_POST_OUT_DIR = "detailed-post-out-dir"
+
+  val HBASE_EXPORT = "hbase-export"
+  val UPLOAD_TO_ES = "upload-to-es"
+  val HBASE_OUT_DIR = "hbase-out-dir"
 }
