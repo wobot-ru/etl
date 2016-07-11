bd85cf0a57127c947981d5af9bc31f59402c6880
Kviz <kviz@outlook.com>
2016-07-11 13:01:18 +0300

    fix. use unique field names through Index classes hierarchy

--- src/main/scala/ru/wobot/etl/dto/PostDto.scala
+++ src/main/scala/ru/wobot/etl/dto/PostDto.scala
@@ -20,11 +20,11 @@ class PostDto(id: String,
 
   def toJson(): String = JsonUtil.toJson(this)
 
-  def copy(id: String = id,
-           segment: String = segment,
-           crawlDate: String = crawlDate,
-           href: String = href,
-           source: String = source,
+  def copy(id: String = this.asInstanceOf[Index].id,
+           segment: String = this.asInstanceOf[Index].segment,
+           crawlDate: String = this.asInstanceOf[Index].crawlDate,
+           href: String = this.asInstanceOf[Index].href,
+           source: String = this.asInstanceOf[Index].source,
            profileId: String = profileId,
            smPostId: String = smPostId,
            parentPostId: String = parentPostId,
